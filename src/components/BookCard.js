8dbe2c8fa6cb771efafea28cbede856d77c2958d
ahmed-asiri <ahmed.su.asiri@gmail.com>
2023-10-09 20:28:01 +0300

    style: add book styles

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,19 +4,28 @@ const BookCard = ({ book }) => {
 	return (
 		<div
 			key={book.id}
-			className="flex flex-col gap-3 border rounded-md p-5"
-			style={{ width: "23.43%" }}>
-			<div className="image">
+			className="relative flex flex-row bg-white gap-3 p-5 border rounded-md shadow-md"
+			style={{ width: "31%" }}>
+			<div className="image flex-auto w-1/2 border shadow">
 				<img
-					className="w-full h-80"
+					className="h-full w-full"
 					src={book.imageLinks.thumbnail}
 					alt={book.title}
 				/>
 			</div>
-			<div>
-				<button className="rounded-md bg-cyan-500 text-white px-5 py-2">
-					Preview
-				</button>
+			<div className="z-1 w-1/2 flex flex-col justify-between">
+				<div>
+					<p className="text-[1.4rem] text-ellipsis line-clamp-2 mb-3">
+						{book.title}
+					</p>
+					<p className="text-[1.15rem] opacity-50 italic text-ellipsis line-clamp-1">
+						{typeof book.authors !== "undefined" &&
+							"By " + book.authors.join(", ")}
+					</p>
+				</div>
+				{/* <p className="text-clip text-[1.2rem] opacity-60 overflow-hidden line-clamp-5 w-full">
+					{book.description}
+				</p> */}
 			</div>
 		</div>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				stone: {
+					900: "#836349",
+					300: "#AF9A83",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
